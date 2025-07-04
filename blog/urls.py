8f1c5808daabeb901ab48a43d412bb13533fed74
yasin aliyari <yasinaliyari30@gmail.com>
2025-07-04 12:54:09 +0330

    Conditionalizing the year digits

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, register_converter, re_path
 from blog.views import post_list, categories_list, post_detail, custom_post_detail
+from blog.utils import FourDigitYear
 
+register_converter(FourDigitYear, 'fourdigit')
 
 urlpatterns = [
     path('list/', post_list),
@@ -8,6 +10,7 @@ urlpatterns = [
     # path('detail/<str:post_title>/', post_detail),
     path('detail/hello/', custom_post_detail),
     path('detail/<slug:post_slug>/', post_detail),
-    path('archive/<int:year>/', post_list),
+    # path('archive/<fourdigit:year>/', post_list),
     path('archive/<int:year>/<int:month>/', post_list),
+    re_path(r"archive/(?P<year>[0-9]{2,4})/", post_list),
 ]
